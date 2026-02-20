9c382391490d36fdddc748dba96ae61ca24e3862
Adrian <adriansga718@gmail.com>
2026-02-20 19:56:06 +0100

    Przywrocono PUSHHOSTING.bat

---
+++ PUSHHOSTING.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+cd /d "G:\Mój dysk\.AI PROJEKTY\Stronyai\.1MOJE\Strony\Hosting"
+git add .
+git commit -m "Aktualizacja %date% %time%"
+git push origin main
+echo.
+echo Gotowe! Strona zaktualizowana.
+pause
