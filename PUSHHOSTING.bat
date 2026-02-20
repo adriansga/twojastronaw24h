8bbed061d944f7132d3335e102b9c7b94679119c
adriansga718 <adriansga718@gmail.com>
2026-02-20 19:33:20 +0100

    Delete PUSHHOSTING.bat

--- PUSHHOSTING.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "G:\Mój dysk\.AI PROJEKTY\Stronyai\.1MOJE\Strony\Hosting"
-git add .                                                                                                              
-git commit -m "Aktualizacja %date% %time%"
-git push origin main
-echo.
-echo Gotowe! Strona zaktualizowana.
-pause
