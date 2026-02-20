487f61181d8bb6a0a92901e6bedd01f4107fdeb8
Adrian <adriansga718@gmail.com>
2026-02-20 19:57:08 +0100

    PUSHHOSTING: dodano git pull przed push

--- PUSHHOSTING.bat
+++ PUSHHOSTING.bat
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001 >nul
 cd /d "G:\Mój dysk\.AI PROJEKTY\Stronyai\.1MOJE\Strony\Hosting"
+git pull origin main --no-rebase
 git add .
 git commit -m "Aktualizacja %date% %time%"
 git push origin main
